9693a6db9b4b764fdaffb1f4c4e4a14f3a94b5c3
Mochammad Bisma Prasetya <moch.bismap@gmail.com>
2026-09-15 14:50:16 +0700

    Ralation Database (Category to id)

--- models.py
+++ models.py
@@ -2,6 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=False)
+    products = db.relationship(
+        "product",
+        backref=False,
+        lazy = True
+    )
+
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
