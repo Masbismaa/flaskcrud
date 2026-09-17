101e9b52e8e976f2ce195fa52193c73c7b5f88a9
Mochammad Bisma Prasetya <moch.bismap@gmail.com>
2026-09-17 13:40:21 +0700

    Feat : Tambah model user dan sistem role

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -37,3 +38,19 @@ class SaleItem(db.Model):
         "Product",
         backref="sale_items"
     )
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password_hash = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20), nullable=False, default="viewer")
+    created_at = db.Column(db.DateTime, nullable=False, server_default = db.func.now())
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(
+            self.password_hash, password
+        )
